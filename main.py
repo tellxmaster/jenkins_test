b5e348fef210e726f655a02c6be9df2192677b61
tellxmaster <telleriaelias@gmail.com>
2023-02-18 11:45:20 -0500

    Separated

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import unittest
 
 def suma(a, b):
     return a + b
@@ -9,23 +8,4 @@ def resta(a, b):
 def multiplicacion(a, b):
     return a * b
 
-class TestFunciones(unittest.TestCase):
-    
-    def test_suma(self):
-        self.assertEqual(suma(2, 3), 5)
-        self.assertEqual(suma(-4, 9), 5)
-        self.assertEqual(suma(0, 0), 0)
-
-    def test_resta(self):
-        self.assertEqual(resta(5, 2), 3)
-        self.assertEqual(resta(0, 0), 0)
-        self.assertEqual(resta(-5, -3), -2)
-
-    def test_multiplicacion(self):
-        self.assertEqual(multiplicacion(2, 3), 6)
-        self.assertEqual(multiplicacion(-4, 9), -36)
-        self.assertEqual(multiplicacion(0, 0), 0)
-
-if __name__ == '__main__':
-    unittest.main()
 
---
+++ tests_file.py
@@ -0,0 +1,23 @@
+import unittest
+from main import suma, resta, multiplicacion;
+
+class TestFunciones(unittest.TestCase):
+    
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
+        self.assertEqual(suma(-4, 9), 5)
+        self.assertEqual(suma(0, 0), 0)
+
+    def test_resta(self):
+        self.assertEqual(resta(5, 2), 3)
+        self.assertEqual(resta(0, 0), 0)
+        self.assertEqual(resta(-5, -3), -2)
+
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicacion(2, 3), 6)
+        self.assertEqual(multiplicacion(-4, 9), -36)
+        self.assertEqual(multiplicacion(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
